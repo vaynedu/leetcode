7b6d0b139835f9cda0edca38db28c272a6c28ff7
vaynedu <1219345363@qq.com>
2025-09-04 09:47:50 +0800

    feat:2.两数相加

---
+++ leetcode/editor/cn/2.add-two-numbers/solution_test.go
@@ -0,0 +1,95 @@
+package leetcode
+
+// 2.两数相加
+
+import (
+	"fmt"
+	"testing"
+)
+
+/**
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+ 请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+ 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+
+
+ 示例 1：
+
+
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+
+
+ 示例 2：
+
+
+输入：l1 = [0], l2 = [0]
+输出：[0]
+
+
+ 示例 3：
+
+
+输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+输出：[8,9,9,9,0,0,0,1]
+
+
+
+
+ 提示：
+
+
+ 每个链表中的节点数在范围 [1, 100] 内
+ 0 <= Node.val <= 9
+ 题目数据保证列表表示的数字不含前导零
+
+
+ Related Topics 递归 链表 数学 👍 11475 👎 0
+
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	cur := dummy
+	carry := 0
+	for l1 != nil || l2 != nil || carry > 0 {
+		cur.Next = &ListNode{}
+		cur = cur.Next
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+		cur.Val = carry % 10
+		carry /= 10
+	}
+	return dummy.Next
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func TestAddTwoNumbers(t *testing.T) {
+	fmt.Println("come on baby !!!")
+	// 生成函数测试用例
+	// 要求 有多组tests，并且有输入值，预期值，如果实际返回值和预期值不同，打印错误日志
+}
