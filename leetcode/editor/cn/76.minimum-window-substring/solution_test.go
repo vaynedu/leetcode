e6c36b641bfb34176ea84af9867ae0db5c18cc85
vaynedu <1219345363@qq.com>
2025-09-11 20:08:14 +0800

    feat:76.最小覆盖子串

--- leetcode/editor/cn/76.minimum-window-substring/solution_test.go
+++ leetcode/editor/cn/76.minimum-window-substring/solution_test.go
@@ -66,7 +66,75 @@ o(m+n) 时间内解决此问题的算法吗？
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func minWindow(s string, t string) string {
+	if len(s) < len(t) {
+		return ""
+	}
 
+	// need 记录字符串t中每个字符需要的数量
+	need := make(map[byte]int)
+	// window 记录当前窗口中每个字符出现的次数
+	window := make(map[byte]int)
+
+	// 初始化need map
+	for i := range t {
+		need[t[i]]++
+	}
+
+	// valid 记录窗口中满足need条件的字符种类数
+	valid := 0
+
+	// 记录最小覆盖子串的起始索引及长度
+	start := 0
+	minLen := len(s) + 1 // 初始化为比s长度大1的值
+
+	// 滑动窗口的左右指针
+	left, right := 0, 0
+
+	for right < len(s) {
+		// c 是将移入窗口的字符
+		c := s[right]
+		// 扩大窗口
+		right++
+
+		// 进行窗口内数据的一系列更新
+		if need[c] > 0 {
+			window[c]++
+			// 如果窗口中字符c的数量达到需要的数量，则valid加1
+			if window[c] == need[c] {
+				valid++
+			}
+		}
+
+		// 判断左侧窗口是否要收缩
+		for valid == len(need) {
+			// 在这里更新最小覆盖子串
+			if right-left < minLen {
+				start = left
+				minLen = right - left
+			}
+
+			// d 是将移出窗口的字符
+			d := s[left]
+			// 缩小窗口
+			left++
+
+			// 进行窗口内数据的一系列更新
+			if need[d] > 0 {
+				// 如果窗口中字符d的数量刚好等于需要的数量，移除后就不满足了，valid减1
+				if window[d] == need[d] {
+					valid--
+				}
+				window[d]--
+			}
+		}
+	}
+
+	// 返回最小覆盖子串
+	if minLen == len(s)+1 {
+		return ""
+	}
+
+	return s[start : start+minLen]
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
