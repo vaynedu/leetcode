66e44b3d2fd87869eda3c7c33207dcbfbaaf3296
vaynedu <1219345363@qq.com>
2026-04-27 15:15:29 +0800

    feat: 131 Palindrome Partitioning (backtracking)

---
+++ interview/131.palindrome-partitioning.go
@@ -0,0 +1,38 @@
+package interview
+
+// 131. 分割回文串
+// 回溯：枚举每个分割点，判断是否为回文
+func partition(s string) [][]string {
+    result := [][]string{}
+    path := []string{}
+    var backtrack func(start int)
+    backtrack = func(start int) {
+        if start == len(s) {
+            tmp := make([]string, len(path))
+            copy(tmp, path)
+            result = append(result, tmp)
+            return
+        }
+        for i := start + 1; i <= len(s); i++ {
+            if isPalindrome(s[start:i]) {
+                path = append(path, s[start:i])
+                backtrack(i)
+                path = path[:len(path)-1]
+            }
+        }
+    }
+    backtrack(0)
+    return result
+}
+
+func isPalindrome(s string) bool {
+    i, j := 0, len(s)-1
+    for i < j {
+        if s[i] != s[j] {
+            return false
+        }
+        i++
+        j--
+    }
+    return true
+}
---
+++ interview/131.palindrome-partitioning_test.go
@@ -0,0 +1,25 @@
+package interview
+
+import "testing"
+
+func TestPartition(t *testing.T) {
+    tests := []struct {
+        name string
+        s string
+        want int
+    }{
+        {"基本", "aab", 2},
+        {"单字符", "a", 1},
+        {"全回文", "aaa", 4},
+        {"无回文", "abc", 1},
+        {"长", "aabb", 4},
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            got := partition(tt.s)
+            if len(got) != tt.want {
+                t.Errorf("partition(%s) returned %d, want %d", tt.s, len(got), tt.want)
+            }
+        })
+    }
+}
