deed944bed74d28c81b458e26eb686a087f61ce1
vaynedu <1219345363@qq.com>
2026-04-27 15:36:57 +0800

    feat: 367 Valid Perfect Square (binary search)

--- interview/367.valid-perfect-square.go
+++ interview/367.valid-perfect-square.go
@@ -1,19 +1,21 @@
 package interview
 
 // 367. 有效的完全平方数
-// 使用 x/mid 判断，避免溢出
+// 二分查找：mid*mid 与 num 比较，注意溢出用除法
 func isPerfectSquare(num int) bool {
     if num < 2 {
         return true
     }
-    left, right := 1, num/2
+    left, right := 2, num/2
     for left <= right {
         mid := left + (right-left)/2
-        if mid <= num/mid {
-            left = mid + 1
-        } else {
+        if mid == num/mid && num%mid == 0 {
+            return true
+        } else if mid > num/mid {
             right = mid - 1
+        } else {
+            left = mid + 1
         }
     }
-    return left*left == num || right*right == num
+    return false
 }
--- interview/367.valid-perfect-square_test.go
+++ interview/367.valid-perfect-square_test.go
@@ -5,19 +5,14 @@ import "testing"
 func TestIsPerfectSquare(t *testing.T) {
     tests := []struct {
         name string
-        num  int
+        num int
         want bool
     }{
-        {"1", 1, true},
-        {"4", 4, true},
-        {"9", 9, true},
         {"16", 16, true},
-        {"0", 0, true},
-        {"2", 2, false},
-        {"3", 3, false},
         {"14", 14, false},
+        {"1", 1, true},
+        {"0", 0, true},
         {"100", 100, true},
-        {"INT_MAX", 2147483647, false},
     }
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
