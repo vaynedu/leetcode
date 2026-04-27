3f11c0cb687fbfbccbc1a17745ec3f8a2fb79c79
vaynedu <1219345363@qq.com>
2026-04-27 15:33:42 +0800

    feat: 2 Add Two Numbers (linked list simulation)

---
+++ interview/2.add-two-numbers.go
@@ -0,0 +1,24 @@
+package interview
+
+// 2. 两数相加
+// 模拟链表加法，注意进位
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    dummy := &ListNode{}
+    cur := dummy
+    carry := 0
+    for l1 != nil || l2 != nil || carry > 0 {
+        sum := carry
+        if l1 != nil {
+            sum += l1.Val
+            l1 = l1.Next
+        }
+        if l2 != nil {
+            sum += l2.Val
+            l2 = l2.Next
+        }
+        carry = sum / 10
+        cur.Next = &ListNode{Val: sum % 10}
+        cur = cur.Next
+    }
+    return dummy.Next
+}
---
+++ interview/2.add-two-numbers_test.go
@@ -0,0 +1,25 @@
+package interview
+
+import "testing"
+
+func TestAddTwoNumbers(t *testing.T) {
+    tests := []struct {
+        name string
+        l1 *ListNode
+        l2 *ListNode
+        want []int
+    }{
+        {"基本", NewList(2, 4, 3), NewList(5, 6, 4), []int{7, 0, 8}},
+        {"不同长度", NewList(9, 9, 9), NewList(1), []int{0, 0, 1}},
+        {"零", NewList(0), NewList(0), []int{0}},
+        {"进位到新节点", NewList(5), NewList(5), []int{0, 1}},
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            got := addTwoNumbers(tt.l1, tt.l2)
+            if !listEqual(got, tt.want) {
+                t.Errorf("addTwoNumbers() = %v, want %v", listToSlice(got), tt.want)
+            }
+        })
+    }
+}
