cfc24515ad9f33d9283063ccb80196f51f4d49ca
vaynedu <1219345363@qq.com>
2026-04-27 15:36:57 +0800

    feat: 162 Find Peak Element (binary search)

--- interview/162.find-peak-element.go
+++ interview/162.find-peak-element.go
@@ -1,8 +1,7 @@
 package interview
 
 // 162. 寻找峰值
-// 峰值是比相邻元素都大的元素，数组两端可以看作负无穷
-// 时间 O(log N)，空间 O(1)
+// 二分查找：找局部峰值，必有一侧递增
 func findPeakElement(nums []int) int {
     left, right := 0, len(nums)-1
     for left < right {
