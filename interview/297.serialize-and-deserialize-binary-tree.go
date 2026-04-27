fd918ebd0ad80ac24cb1979e6329ce828cee99d0
vaynedu <1219345363@qq.com>
2026-04-27 15:38:29 +0800

    feat: 297 Serialize and Deserialize Binary Tree

---
+++ interview/297.serialize-and-deserialize-binary-tree.go
@@ -0,0 +1,48 @@
+package interview
+
+import (
+    "strconv"
+    "strings"
+)
+
+// 297. 二叉树的序列化与反序列化
+// 前序遍历序列化，空节点用 # 分隔
+type Codec struct{}
+
+func Constructor() Codec {
+    return Codec{}
+}
+
+func (c *Codec) serialize(root *TreeNode) string {
+    if root == nil {
+        return "#"
+    }
+    left := c.serialize(root.Left)
+    right := c.serialize(root.Right)
+    return strconv.Itoa(root.Val) + "," + left + "," + right
+}
+
+func (c *Codec) deserialize(data string) *TreeNode {
+    if data == "" {
+        return nil
+    }
+    nodes := strings.Split(data, ",")
+    idx := 0
+    var build func() *TreeNode
+    build = func() *TreeNode {
+        if idx >= len(nodes) {
+            return nil
+        }
+        val := nodes[idx]
+        idx++
+        if val == "#" {
+            return nil
+        }
+        v, _ := strconv.Atoi(val)
+        node := &TreeNode{Val: v}
+        node.Left = build()
+        node.Right = build()
+        return node
+    }
+    return build()
+}
